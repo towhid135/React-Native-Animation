f4a1a98b894dd368ed262525c09850c106bd85f5
towhid135 <17101135@uap-bd.edu>
2022-02-25 16:30:54 +0600

    reanimated initiated

--- App.js
+++ App.js
@@ -1,7 +1,16 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Animated,{useSharedValue,useAnimatedStyle} from 'react-native-reanimated';
 
+const SIZE = 100.0;
 export default function App() {
+  const progress = useSharedValue(0);
+  const reanimatedStyle = useAnimatedStyle(() =>{
+    return {
+      opacity: progress.value,
+    }
+  },[])
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
